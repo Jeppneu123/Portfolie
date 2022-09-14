1120c26666eebf2bb52900f9141ad7b37e7f9530
Jeppeneu123 <jeppeneumann@live.dk>
2022-09-14 08:43:05 +0200

    Videre på hjemmeside Index.html

---
+++ index-kopi.js
@@ -0,0 +1,20 @@
+let navbar = document.getElementById("navbar")
+let navbar = document.getElementById(main-nav);
+let navPos = navbar.getBoundingClientRect().top;
+
+window.addEventListener("scroll", e =>
+{
+    let scrollPos = window.scrollY;
+    if (scrollPos > navPos) {
+        navbar.classList.add("sticky");
+    }
+header.classList.add(`navbarOffsetMargin`);
+    } else
+{
+    navbar.classList.remove(`sticky`);
+
+    header.classList.remove(`navbarOffsetMargin`);
+    }
+});
+
+
